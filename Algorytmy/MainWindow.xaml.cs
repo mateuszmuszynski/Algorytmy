e10cfb10f836188b1a479b6cb24819273f1dae18
mateuszmuszynski <mateuszmuszynski92@gmail.com>
2017-05-21 11:01:33 +0200

    Added UI map for generic heuristic

--- Algorytmy/MainWindow.xaml.cs
+++ Algorytmy/MainWindow.xaml.cs
@@ -346,5 +346,151 @@ namespace Algorytmy
             });
             t.Stop();
         }
+
+        private void calculateButtonMapGenetic_Click(object sender, RoutedEventArgs e)
+        {
+            PathResult result;
+
+            var hillClimbing = new HillClimbing();
+
+            List<Coordinate> data;
+
+            var numberOfElementsToTake = int.Parse(elementsToTakeTextbox.Text);
+
+            var cities = new List<City>
+                {
+                    new City
+                    {
+                        Name = "Bialystok",
+                        Coordinate = new Coordinate
+                        {
+                            X = 53.13248859999999,
+                            Y = 23.1688403
+                        }
+                    },
+                    new City
+                    {
+                        Name = "Warszawa",
+                        Coordinate = new Coordinate
+                        {
+                            X = 52.2296756,
+                            Y = 21.0122287
+                        }
+                    },
+                    new City
+                    {
+                        Name = "Krakow",
+                        Coordinate = new Coordinate
+                        {
+                            X = 50.06465009999999,
+                            Y = 19.9449799
+                        }
+                    },
+                    new City
+                    {
+                        Name = "Lodz",
+                        Coordinate = new Coordinate
+                        {
+                            X = 51.7592485,
+                            Y = 19.4559833
+                        }
+                    },
+                    new City
+                    {
+                        Name = "Wroclaw",
+                        Coordinate = new Coordinate
+                        {
+                            X = 51.1078852,
+                            Y = 17.0385376
+                        }
+                    },
+                    new City
+                    {
+                        Name = "Poznan",
+                        Coordinate = new Coordinate
+                        {
+                            X = 52.406374,
+                            Y = 16.9251681
+                        }
+                    },
+                    new City
+                    {
+                        Name = "Gdansk",
+                        Coordinate = new Coordinate
+                        {
+                            X = 54.35202520000001,
+                            Y = 18.6466384
+                        }
+                    },
+                    new City
+                    {
+                        Name = "Szczecin",
+                        Coordinate = new Coordinate
+                        {
+                            X = 53.4285438,
+                            Y = 14.5528116
+                        }
+                    },
+                    new City
+                    {
+                        Name = "Bydgoszcz",
+                        Coordinate = new Coordinate
+                        {
+                            X = 53.12348040000001,
+                            Y = 18.0084378
+                        }
+                    },
+                    new City
+                    {
+                        Name = "Lublin",
+                        Coordinate = new Coordinate
+                        {
+                            X = 51.2464536,
+                            Y = 22.5684463
+                        }
+                    }
+                };
+
+
+            var loader = new DataLoader(@"..\..\daneNasze.txt", NumberStyles.Float);
+            data = loader.GetData();
+
+            var selectedName = ((ComboBoxItem)cityGeneticComboBox.SelectedValue).Content;
+
+            var currentCity = cities.First(x => x.Name == (string)selectedName);
+
+            result = hillClimbing.GetPath(currentCity.Coordinate, data, numberOfElementsToTake);
+
+            mapGeneticPolygon.Locations.Clear();
+
+
+            var childsToRemove = mapGenetic.Children.OfType<Pushpin>().Cast<UIElement>().ToList();
+
+            foreach (var child in childsToRemove)
+            {
+                mapGenetic.Children.Remove(child);
+            }
+
+            var pin = new Pushpin();
+            pin.Location = new Location(currentCity.Coordinate.X, currentCity.Coordinate.Y);
+            mapGenetic.Children.Add(pin);
+
+            foreach (var item in result.Path)
+            {
+                var location = data[item];
+
+                mapGeneticPolygon.Locations.Add(new Location(location.X, location.Y));
+            }
+
+            distanceLabelMapGenetic.Content = "Distance: " + result.Distance;
+
+            var fileData = result.Path.Select(x => (x + 1).ToString()).Aggregate((current, next) => current + " " + next);
+            var fileName = @"D:\result" + string.Format("{0:yyyy-MM-dd_hh-mm-ss}", DateTime.Now) + ".txt";
+
+            StreamWriter file = new System.IO.StreamWriter(fileName, true);
+            file.WriteLine(fileData);
+
+            file.Close();
+        }
     }
 }
